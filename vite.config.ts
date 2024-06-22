1e9f461c814c2e637f6e1c2cecdfd0631223c9fc
keisokoo <keisokoo@gmail.com>
2024-06-22 14:40:07 +0900

    deploy2

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/spa-remix/",
   plugins: [
     remix({
+      basename: "/spa-remix/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
